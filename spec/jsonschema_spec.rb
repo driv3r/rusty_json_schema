abbc346d7fa7289f9f5b3a16ae37ca03d5db9311
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-18 16:39:13 +0100

    Bootstrap gem

---
+++ spec/jsonschema_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jsonschema do
+  it "has a version number" do
+    expect(Jsonschema::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
