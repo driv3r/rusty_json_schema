8e136f08559116613c3a8f2378262702c9016177
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-27 19:12:02 +0100

    Bump changelog, version & added benchmark vs json schemer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.0]
+### Added
+- Basic FFI bindings with simple validation implementation. Ensured that memory usage is ok.
+
 ## [0.0.1]
 ### Added
 - basic gem structure
---
+++ spec/benchmark.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "benchmark/ips"
+require "json_schemer"
+require "json_schema-rs"
+require "json"
+big_schema = File.read("spec/fixtures/canada_schema.json")
+big_schema_hash = JSON.parse(big_schema)
+big_event = File.read("spec/fixtures/canada.json")
+big_event_hash = JSON.parse(big_event)
+
+small_schema = File.read("spec/fixtures/small_schema.json")
+small_schema_hash = JSON.parse(small_schema)
+small_event = File.read("spec/fixtures/small_valid.json")
+small_event_hash = JSON.parse(small_event)
+small_invalid_event = File.read("spec/fixtures/small_invalid.json")
+small_invalid_event_hash = JSON.parse(small_invalid_event)
+
+big_validator = JsonSchema.build(big_schema)
+big_schemer = JSONSchemer.schema(big_schema_hash)
+small_validator = JsonSchema.build(small_schema)
+small_schemer = JSONSchemer.schema(small_schema_hash)
+
+Benchmark.ips do |x|
+  x.config(time: 30, warmup: 6)
+
+  x.report("big validator") { raise "foo" unless big_validator.valid?(big_event) }
+  x.report("big schemer") { raise "foo" unless big_schemer.valid?(big_event_hash) }
+
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("small validator: valid") { raise "foo" unless small_validator.valid?(small_event) }
+  x.report("small schemer:   valid") { raise "foo" unless small_schemer.valid?(small_event_hash) }
+
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("small validator: invalid") { raise "foo" if small_validator.valid?(small_invalid_event) }
+  x.report("small schemer:   invalid") { raise "foo" if small_schemer.valid?(small_invalid_event_hash) }
+
+  x.compare!
+end
