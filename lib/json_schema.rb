bf2ffd735741940fee6a3afc4aa43d1369b187fa
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-27 18:13:21 +0100

    Basic implementation

--- lib/json_schema.rb
+++ lib/json_schema.rb
@@ -1,10 +1,64 @@
 # frozen_string_literal: true
 
+require "ffi"
+
 require_relative "json_schema/version"
 
+# Json Schema validation
+#
+# ## Example
+#
+#     validator = JsonSchema.build(schema) # where schema is a json string of the schema
+#     validator.valid?(event) # where event is a json string of schema
+#     # => true/false
+#
 module JsonSchema
 
+  def self.build(schema)
+    JsonSchema::Validator::Binding.new(schema)
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Handles release of the pointer automatically
+  # with Ruby GC. This way we can intialize validator
+  # in Rust, and hold a reference in Ruby.
+  #
+  class Validator < FFI::AutoPointer
+
+    # Custom GC flow for our validator, freeing
+    # the object within Rust
+    #
+    def self.release(pointer)
+      Binding.free(pointer)
+    end
+
+    # Simple validation without actual error messages
+    #
+    def valid?(event)
+      Binding.is_valid(self, event)
+    end
+
+    # FFI container for our library.
+    #
+    module Binding
+
+      extend FFI::Library
+
+      lib_name =
+        case ::FFI::Platform::LIBSUFFIX
+        when "so", "dylib" then "libjson_schema"
+        when "dll" then "json_schema"
+        end
+
+      ffi_lib File.expand_path("ext/#{lib_name}.#{::FFI::Platform::LIBSUFFIX}", __dir__)
+
+      attach_function :new, :validator_new, [:string], Validator
+      attach_function :free, :validator_free, [Validator], :void
+      attach_function :is_valid, :validator_is_valid, [Validator, :string], :bool
+
+    end
+
+  end
 
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,118 @@
+use jsonschema::JSONSchema;
+use serde_json::Value;
+
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+/*
+ * Our wrapper struct for schema and schema value,
+ * we need to hold onto value in order to not have
+ * it freed up, as JSONSchema uses it as reference.
+ */
+pub struct Validator {
+    schema: &'static JSONSchema<'static>,
+    schema_value: &'static Value,
+}
+
+impl Validator {
+    /*
+     * With Box::leak we avoid freeing up of schema
+     * and schema value, we free them up separately
+     * in the Drop implementation
+     */
+    fn new(schema: Value) -> Validator {
+        let boxed_schema: &'static Value = Box::leak(Box::new(schema));
+        let boxed_compile: &'static JSONSchema<'static> = Box::leak(Box::new(JSONSchema::compile(boxed_schema).unwrap()));
+
+        Validator {
+            schema: boxed_compile,
+            schema_value: boxed_schema,
+        }
+    }
+
+    fn is_valid(&self, event: &Value) -> bool {
+        self.schema.is_valid(event)
+    }
+}
+
+impl Drop for Validator {
+    /*
+     * Free up schema with value by "materializing" them,
+     * otherwise they will leak memory.
+     */
+    fn drop(&mut self) {
+        unsafe {
+            Box::from_raw(self.schema as *const _ as *mut JSONSchema);
+            Box::from_raw(self.schema_value as *const _ as *mut Value);
+        }
+    }
+}
+
+fn to_string(ptr: *const c_char) -> &'static CStr {
+    unsafe {
+        assert!(!ptr.is_null());
+        CStr::from_ptr(ptr)
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn validator_new(c_schema: *const c_char) -> *mut Validator {
+    let raw_schema  = to_string(c_schema);
+    let schema = serde_json::from_slice(raw_schema.to_bytes()).unwrap();
+    let validator = Validator::new(schema);
+
+    Box::into_raw(Box::new(validator))
+}
+
+#[no_mangle]
+pub extern "C" fn validator_free(ptr: *mut Validator) {
+    if ptr.is_null() {
+        return;
+    }
+
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn validator_is_valid(ptr: *const Validator, event: *const c_char) -> bool {
+    let validator = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+
+    let raw_event = to_string(event);
+    let event: Value = serde_json::from_slice(raw_event.to_bytes()).unwrap();
+
+    validator.is_valid(&event)
+}
+
 #[cfg(test)]
 mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use std::ffi::CString;
+    use std::{thread, time};
+
+    /*
+     * Simple sanity check if everything works together
+     */
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_valid_event() {
+        let schema = CString::new("{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"type\":\"array\",\"items\":[{\"type\":\"number\",\"exclusiveMaximum\":10}]}").unwrap();
+        let valid_event = CString::new("[9]").unwrap();
+        let invalid_event = CString::new("[22]").unwrap();
+
+        let c_schema_ptr: *const c_char = schema.as_ptr();
+        let c_valid_event_ptr: *const c_char = valid_event.as_ptr();
+        let c_invalid_event_ptr: *const c_char = invalid_event.as_ptr();
+
+        let validator = validator_new(c_schema_ptr);
+
+        assert!(validator_is_valid(validator, c_valid_event_ptr));
+        assert!(!validator_is_valid(validator, c_invalid_event_ptr));
+
+        validator_free(validator);
     }
 }
