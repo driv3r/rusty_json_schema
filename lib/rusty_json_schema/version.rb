2b5256815dd49c4ac7d5ff2a65780703eed73a18
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-28 10:06:30 +0100

    Rename to RustyJSONSchema to avoid collisions

--- lib/rusty_json_schema/version.rb
+++ lib/rusty_json_schema/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module JsonSchema
+module RustyJSONSchema
 
   VERSION = "0.1.0"
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "json_schema-rs"
+require "rusty_json_schema"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
