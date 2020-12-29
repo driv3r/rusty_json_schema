dadff3466101e56a8a6670a11cff415dc48ca6cf
Leszek Zalewski <leszekzalewski@fastmail.fm>
2020-12-29 12:37:25 +0100

    Add tests for validate method

--- spec/rusty_json_schema_spec.rb
+++ spec/rusty_json_schema_spec.rb
@@ -31,4 +31,43 @@ RSpec.describe RustyJSONSchema do
       end
     end
   end
+
+  describe "#validate" do
+    subject(:validator) { described_class.build(schema) }
+
+    let(:schema) do
+      {
+        "properties": {
+          "foo": {
+            type: "string"
+          },
+          "bar": {
+            type: "number"
+          },
+          "baz": {}
+        },
+        "required": ["baz"]
+      }
+    end
+
+    context "when valid event" do
+      let(:event) { { "foo" => "rusty", "bar" => 1, "baz" => "rusty" } }
+
+      it "returns empty array" do
+        expect(validator.validate(event)).to eq []
+      end
+    end
+
+    context "when invalid event" do
+      let(:event) { { "foo" => 1, "bar" => "rusty" } }
+
+      it "returns validation errors" do
+        expect(validator.validate(event)).to eq [
+          "'\"rusty\"' is not of type 'number'",
+          "'1' is not of type 'string'",
+          "'baz' is a required property"
+        ]
+      end
+    end
+  end
 end
